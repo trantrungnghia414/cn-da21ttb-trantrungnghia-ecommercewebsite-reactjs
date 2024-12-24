c892d1f34fe7cd615b3b17762bc9b9a745cb2ff3
Trung Nghia <nghiatrandtnt@gmail.com>
2024-12-24 14:34:23 +0700

    update backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
 // Middleware, HTTP logger
 app.use(morgan("combined"));
 
-// Template engine
-app.engine("handlebars", handlebars());
-app.set("view engine", "handlebars");
-
 app.get("/", (req, res) => {
-    res.send(123);
+    res.send('123');
 });
 
 app.listen(port, () => {
